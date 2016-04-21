7d035563e59aa9cc03184f7217396eacac12a072
Wang <WangC@ad.umassmed.edu>
2016-04-21 14:29:04 -0400

    revised 4/21/2016

--- src/main/scala/outlierDetection.scala
+++ src/main/scala/outlierDetection.scala
@@ -8,35 +8,55 @@ import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.Encoders
 import scala.util.matching.Regex
+import java.io.File
+import java.io.File._
+import java.util.Scanner
+import java.io._
 
-case class DataItem(Alcohol:Double, Malicacid:Double,Ash:Double,
-        AlcalinityOfash:Double,Magnesium:Double, TotalPhenols:Double,
-        Flavanoids:Double, NonflavanoidPhenols:Double, Proanthocyanins:Double,
-        ColorIntensity:Double,Hue:Double, dilutedWines:Double, Proline:Double );
-//case class DataItem(id:Int,Z:Double,Y:Double,X:Double,celsius:Double,eda:Double);
+case class DataItem(year:String,month:String,day:String,hour:String,
+		Z:Double,Y:Double,X:Double,celsius:Double,eda:Double);
 case class Window(width:Double,slide:Int);
-case class DataPoint(id:Int,preNeig:Seq[Int],succNeigNum:Int,expTime:Double);
+case class DataPoint(id:String,preNeig:Seq[String],succNeigNum:Int,expTime:Double);
 case class Event(time:Int,id:Int);
+case class OutlierParam(R:Double,k:Int);    //R radius, k number of neighbors;
 
 object outlierDetection {
   
   def main(args:Array[String]){
-    val nn="D://UmassMed//Code//Dataset//wine//wine.data";
-    val window=Window(12.0,1);  //width=12.0,slide=1;
-    val conf = new SparkConf().setAppName("WhereIsOutlier")
-    		.setMaster("local[2]")
-    		.setAppName("WhereIsOutlier");
-    val sc = new SparkContext(conf);
-    val sqlContext = new org.apache.spark.sql.SQLContext(sc);
-    
-   
-    //val ds = sqlContext.read.text(nn).as[DataItem];     
+	  val dataDir="C://Users//wangc//DataSet//CleanedData";
+	  val window=Window(12.0,1);  //width=12.0,slide=1;
+	  val outlierParam=OutlierParam(12.62,6);
+	  val conf = new SparkConf().setAppName("WhereIsOutlier")
+			  .setMaster("local[2]")
+			  .setJars(Seq("/a/b/x.jar", "/c/d/y.jar"));
+	  val sc = new SparkContext(conf);
+	  val sqlContext = new org.apache.spark.sql.SQLContext(sc);
+	  cod(dataDir,sqlContext,window,outlierParam);
+	  //job4Clean(sqlContext);
+  }
+  def job4Clean(sqlContext:SQLContext){
+    val srcDirName="C://Users//wangc//DataSet//CleanedData//temp";
+	  var objDir="C://Users//wangc//DataSet//CleanedData";
+	  val tempDir=new File(srcDirName);
+	  if (tempDir.exists && tempDir.isDirectory){
+		  for (curDir<- tempDir.listFiles){
+			  var curDirName=curDir.getName;    
+			  val srcFile=srcDirName+"//"+curDir.getName;
+			  //val objFile=curDir.getName;
+			  val objFile=objDir+"//"+curDir.getName;
+			  clean(srcFile,objFile,sqlContext); 
+		  }
 
-   
-    /*
+	  }
+  }
+  def cod(dataDir:String,sqlContext:SQLContext,window:Window,outlierParam:OutlierParam){
+    import sqlContext.implicits._;
+    val dataFile=dataDir+"//Patient1.csv//"+"part-00000";
+	  val ds = sqlContext.read.text(dataFile).as[DataItem];     
     var evtQue=Seq(Event(0,0)).toDS;
     var firstDataItem=Seq(ds.first).toDS;
-    var pt=DataPoint(ds.first.id,Seq(ds.first.id),0,window.width);
+    var id=ds.first.year+ds.first.month+ds.first.day+ds.first.hour;
+    var pt=DataPoint(id,Seq(id),0,window.width+0);
     var ptInWindow=Seq(pt).toDS;
     var leftDs=ds.subtract(firstDataItem);
    
@@ -53,10 +73,110 @@ object outlierDetection {
     	//val t = KDTree.fromSeq();
     	firstDataItem=Seq(ds.first).toDS;
     	leftDs=leftDs.subtract(firstDataItem)
-    }*/
+    }
   }
-  def clean(srcFile:String,objFile:String,sqlContext:SQLContext){
-	  //val sqlContext = new SQLContext(sc);
+  def sortAndAgg(srcFile:String,objFile:String,sqlContext:SQLContext)  {    
+  
+	  val df = sqlContext.read
+			  .format("com.databricks.spark.csv")
+			  .option("header", "true") // Use first line of all files as header
+			  .option("inferSchema", "true") // Automatically infer data types
+			  .load(srcFile);
+	  val tdf=df.groupBy("year","month","day","hour")
+			  .agg(avg(df.col("Celsius")),avg(df.col("Z-axis")),avg(df.col("Y-axis")),avg(df.col("X-axis")),
+					  avg(df.col("EDA")));
+
+	  val sortedTdf=tdf.orderBy("year","month","day","hour").save(objFile,"com.databricks.spark.csv");   
+
+  }
+  def cleanSingleFile(dataDir:String,objDir:String,objFileName:String) {	  
+	  //val dataDir="D://Wangjin//UmassMed//Code//Dataset//cocaine//00001CompletedCSVfiles"
+	  val d = new File(dataDir)
+	  //var objFileName:String="patient1.csv"
+	  val writer = new PrintWriter(new File(objDir+"//"+objFileName))
+	  //var startTime:Int=0;
+	  var year:String="";
+	  var month:String="";
+	  var day:String="";
+	  var hour:Int=0;
+	  var minute:Int=0;
+	  var seconds:Double=0;
+
+	  if (d.exists && d.isDirectory) {
+		  val tempFiles=d.listFiles.toList
+				  var writeHeader:Boolean=true;
+		  //println(tempFiles.toString)
+		  tempFiles.foreach{tf=>
+		  //println(tf)						
+		  if (tf.getName.matches("\\d.+\\.csv\\b")){
+			  println(tf.toString)
+			  val bufferedSource = scala.io.Source.fromFile(tf.toString);
+			  var lineCount:BigInt=0;
+			  var flag=false;
+			  for (line<-bufferedSource.getLines){
+				  //println(line)
+				  lineCount+=1;
+				  if (lineCount==6){
+					  //println(line)
+					  val pattern1=new Regex("\\d{4}-\\d{2}-\\d{2}");
+					  val pattern2=new Regex("\\d{2}:\\d{2}:\\d{2}");
+					  val startDate=(pattern1 findAllIn line).mkString(",").split("-");								  
+					  year=startDate(0)
+							  month=startDate(1)
+							  day=startDate(2)
+							  //objFileName=objFileName+startDate
+							  //println(objFileName);
+							  val startTime=(pattern2 findAllIn line).mkString(",").split(":");
+					  hour=startTime(0).toInt
+							  minute=startTime(1).toInt
+							  //startTime=tStartTime.substring(0,2).toInt
+							  //println(startTime);									   
+
+				  } else if (lineCount==7 && writeHeader==true){
+					  writer.write("year,month,day,hour,minute,seconds,Time,Z-axis,Y-axis,X-axis,Battery,Celsius,EDA,Event"+"\n");
+					  writeHeader=false;
+				  } else if (lineCount>8){
+					  val cols = line.split(",").map(_.trim);
+					  val tDate=cols(0).split(":");
+					  //println(line)
+					  hour=tDate(0).toInt;
+					  minute=tDate(1).toInt;
+					  seconds=tDate(2).toDouble;					  
+					  writer.write(year+","+month+","+day+","+hour+","+minute+","+seconds+","+line+"\n");
+				  }
+			  }
+			  bufferedSource.close   
+
+		  }
+		  }		     
+
+	  }
+	  writer.close
+  }
+  def cleanMultiple(srcDir:String,objDir:String,objPrefix:String) {  
+
+	  val srcDirFile=new File(srcDir);
+	  if (srcDirFile.exists && srcDirFile.isDirectory) {
+		  val tempFiles=srcDirFile.listFiles.toList;
+		  for (fileIter<-tempFiles){
+			  val tPattern=new Regex("\\d{5}");
+			  var subFileName=fileIter.getName;
+			  var objFilePattern= tPattern findFirstIn subFileName;
+			  if (!objFilePattern.isEmpty){
+				  var objFileName=objPrefix + objFilePattern.mkString(",").toInt+".csv";
+				  println(objFileName)
+				  cleanSingleFile(fileIter.toString,objDir,objFileName);  
+			  }			   
+
+		  }
+	  }
+
+  }
+  
+   
+   
+  
+  def clean(srcFile:String,objFile:String,sqlContext:SQLContext){	  
 	  val df = sqlContext.read
 			  .format("com.databricks.spark.csv")
 			  .option("delimiter",",")
@@ -64,13 +184,15 @@ object outlierDetection {
 			  .option("inferSchema", "true") // Automatically infer data types
 			  .load(srcFile);   //"cars.csv"
 	  val newDf=df.groupBy("year","month","day","hour")
-			  .agg(avg(col("Z_axis")), avg(col("Y_axis")),avg(col("X_axis")),
-	      avg(col("Celsius")),avg(col("EDA"))
-	      ).sort("year", "month","day","hour");
+			  .agg(avg(col("Z-axis")), avg(col("Y-axis")),avg(col("X-axis")),
+	      avg(col("Celsius")),avg(col("EDA")),avg(col("Battery")),avg(col("Event")))
+	      .sort("year", "month","day","hour");
 	  
-	  newDf.write
-	  .format("com.databricks.spark.csv")
-	  .option("header", "true")
+	  newDf                    
+	  .coalesce(1)
+	  .write
+	  .format("com.databricks.spark.csv")	  
+	  .option("header", "true")		  
 	  .save(objFile);  //"newcars.csv"
   }
   def myRead[T](srcFile:String,delimiter:String,sqlContext:SQLContext) {
